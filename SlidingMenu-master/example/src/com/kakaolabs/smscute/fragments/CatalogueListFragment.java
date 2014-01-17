8c2624860193f34e2674c1a7e570e93c21732991
dungnh8 <dungnh8@localhost.localdomain>
2014-01-17 15:02:57 +0700

    add expandable listview

--- SlidingMenu-master/example/src/com/kakaolabs/smscute/fragments/CatalogueListFragment.java
+++ SlidingMenu-master/example/src/com/kakaolabs/smscute/fragments/CatalogueListFragment.java
@@ -1,63 +1,82 @@
 package com.kakaolabs.smscute.fragments;
 
-import android.content.Context;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
 
 import com.kakaolabs.smscute.R;
+import com.kakaolabs.smscute.adapter.ExpandableListAdapter;
 
-public class CatalogueListFragment extends ListFragment {
+public class CatalogueListFragment extends Fragment {
+	ExpandableListAdapter listAdapter;
+	ExpandableListView expListView;
+	List<String> listDataHeader;
+	HashMap<String, List<String>> listDataChild;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.catalogue_list_layout, null);
+		View view = inflater.inflate(R.layout.catalogue_list_layout, null);
+		// get the listview
+		expListView = (ExpandableListView) view.findViewById(R.id.lvExp);
+		// preparing list data
+		prepareListData();
+		listAdapter = new ExpandableListAdapter(this.getActivity(),
+				listDataHeader, listDataChild);
+		// setting list adapter
+		expListView.setAdapter(listAdapter);
+		return view;
+	}
+
+	/*
+	 * Preparing the list data
+	 */
+	private void prepareListData() {
+		listDataHeader = new ArrayList<String>();
+		listDataChild = new HashMap<String, List<String>>();
+
+		// Adding child data
+		listDataHeader.add("Top 250");
+		listDataHeader.add("Now Showing");
+		listDataHeader.add("Coming Soon..");
+
+		// Adding child data
+		List<String> top250 = new ArrayList<String>();
+		top250.add("The Shawshank Redemption");
+		top250.add("The Godfather");
+		top250.add("The Godfather: Part II");
+		top250.add("Pulp Fiction");
+		top250.add("The Good, the Bad and the Ugly");
+		top250.add("The Dark Knight");
+		top250.add("12 Angry Men");
+
+		List<String> nowShowing = new ArrayList<String>();
+		nowShowing.add("The Conjuring");
+		nowShowing.add("Despicable Me 2");
+		nowShowing.add("Turbo");
+		nowShowing.add("Grown Ups 2");
+		nowShowing.add("Red 2");
+		nowShowing.add("The Wolverine");
+
+		List<String> comingSoon = new ArrayList<String>();
+		comingSoon.add("2 Guns");
+		comingSoon.add("The Smurfs 2");
+		comingSoon.add("The Spectacular Now");
+		comingSoon.add("The Canyons");
+		comingSoon.add("Europa Report");
+
+		listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+		listDataChild.put(listDataHeader.get(1), nowShowing);
+		listDataChild.put(listDataHeader.get(2), comingSoon);
 	}
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		SampleAdapter adapter = new SampleAdapter(getActivity());
-		for (int i = 0; i < 20; i++) {
-			adapter.add(new SampleItem("Sample List",
-					android.R.drawable.ic_menu_search));
-		}
-		setListAdapter(adapter);
-	}
-
-	private class SampleItem {
-		public String tag;
-		public int iconRes;
-
-		public SampleItem(String tag, int iconRes) {
-			this.tag = tag;
-			this.iconRes = iconRes;
-		}
-	}
-
-	public class SampleAdapter extends ArrayAdapter<SampleItem> {
-
-		public SampleAdapter(Context context) {
-			super(context, 0);
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if (convertView == null) {
-				convertView = LayoutInflater.from(getContext()).inflate(
-						R.layout.row, null);
-			}
-			ImageView icon = (ImageView) convertView
-					.findViewById(R.id.row_icon);
-			icon.setImageResource(getItem(position).iconRes);
-			TextView title = (TextView) convertView
-					.findViewById(R.id.row_title);
-			title.setText(getItem(position).tag);
-
-			return convertView;
-		}
 	}
 }
