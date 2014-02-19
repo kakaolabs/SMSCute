b6c7bb60ca358bd6f43491cae5df1d0975862f73
dungnh8 <dungnh8@localhost.localdomain>
2014-02-19 15:12:02 +0700

    change smscute project name

--- SlidingMenu-master/SMSCute/src/com/kakaolabs/smscute/SMSDetailActivity.java
+++ SlidingMenu-master/SMSCute/src/com/kakaolabs/smscute/SMSDetailActivity.java
@@ -3,7 +3,6 @@ package com.kakaolabs.smscute;
 import java.util.ArrayList;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -13,14 +12,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kakaolabs.smscute.adapter.SMSFragmentAdapter;
-import com.kakaolabs.smscute.database.MySQLiteHelper;
 import com.kakaolabs.smscute.database.table.SMS;
-import com.kakaolabs.smscute.helper.DrawHelper;
-import com.kakaolabs.smscute.listener.SMSDetailListener;
 import com.kakaolabs.smscute.util.Constants;
 
-public class SMSDetailActivity extends FragmentActivity implements
-		SMSDetailListener {
+public class SMSDetailActivity extends FragmentActivity {
 	private static final String TAG = "SMSDetailActivity";
 	private SMS sms;
 	private ArrayList<SMS> smsList;
@@ -28,7 +23,7 @@ public class SMSDetailActivity extends FragmentActivity implements
 	private String catalogueTitle;
 	private SMSFragmentAdapter mAdapter;
 	private ViewPager mPager;
-	private ImageView backButton, sendButton, favoriteButton;
+	private ImageView backButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -38,7 +33,6 @@ public class SMSDetailActivity extends FragmentActivity implements
 			setContentView(R.layout.sms_detail_layout);
 			setComponentView();
 			setListener();
-			setImageFavorite(sms);
 		} catch (Exception e) {
 			Log.e(TAG, "onCreate", e);
 		}
@@ -47,13 +41,11 @@ public class SMSDetailActivity extends FragmentActivity implements
 	@Override
 	public void onResume() {
 		super.onResume();
-		DrawHelper.addSMSDetailListener(this);
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
-		DrawHelper.removeSMSDetailListener(this);
 	}
 
 	/**
@@ -70,8 +62,6 @@ public class SMSDetailActivity extends FragmentActivity implements
 		mPager.setCurrentItem(position);
 		// button
 		backButton = (ImageView) findViewById(R.id.header_back_button);
-		sendButton = (ImageView) findViewById(R.id.header_send_button);
-		favoriteButton = (ImageView) findViewById(R.id.header_favorite_button);
 		// header
 		TextView title = (TextView) findViewById(R.id.header_title);
 		title.setText(catalogueTitle);
@@ -85,8 +75,6 @@ public class SMSDetailActivity extends FragmentActivity implements
 	 */
 	private void setListener() {
 		setBackListener();
-		setSendListener();
-		setFavoriteListener();
 	}
 
 	/**
@@ -105,69 +93,11 @@ public class SMSDetailActivity extends FragmentActivity implements
 	}
 
 	/**
-	 * set send listener
-	 * 
-	 * @author dungnh8
-	 */
-	private void setSendListener() {
-		sendButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				int currentPosition = mPager.getCurrentItem();
-				SMS currentSMS = smsList.get(currentPosition);
-				Log.i(TAG, currentSMS.toString());
-				// set sms to be used
-				SMS[] smsParams = { currentSMS };
-				new UpdateUsedSMSAsyncTask().execute(smsParams);
-				// send sms
-				Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
-				intent.putExtra("sms_body", currentSMS.getContent());
-				intent.setType("vnd.android-dir/mms-sms");
-				startActivity(intent);
-			}
-		});
-	}
-
-	/**
-	 * set sms to be favorite
-	 * 
-	 * @author dungnh8
-	 */
-	private void setFavoriteListener() {
-		favoriteButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				int currentPosition = mPager.getCurrentItem();
-				SMS currentSMS = smsList.get(currentPosition);
-				boolean isFavorited = !currentSMS.isFavorited();
-				currentSMS.setFavorited(isFavorited);
-				setImageFavorite(currentSMS);
-				SMS[] smsParams = { currentSMS };
-				new UpdateFavoritedSMSAsyncTask().execute(smsParams);
-			}
-		});
-	}
-
-	/**
-	 * set image to be favorite or not
-	 * 
-	 * @author dungnh8
-	 */
-	private void setImageFavorite(SMS currentSMS) {
-		if (currentSMS.isFavorited()) {
-			favoriteButton.setImageResource(R.drawable.ic_favorite_check);
-		} else {
-			favoriteButton.setImageResource(R.drawable.ic_favorite);
-		}
-	}
-
-	/**
 	 * get data from bundle
 	 * 
 	 * @author dungnh8
 	 */
+	@SuppressWarnings("unchecked")
 	private void getDataFromBundle() {
 		try {
 			Intent intent = getIntent();
@@ -180,45 +110,5 @@ public class SMSDetailActivity extends FragmentActivity implements
 		} catch (Exception e) {
 			Log.e(TAG, "getDataFromBundle", e);
 		}
-	}
-
-	/**
-	 * update this sms to be used
-	 * 
-	 * @author dungnh8
-	 * 
-	 */
-	private class UpdateUsedSMSAsyncTask extends AsyncTask<SMS, Void, Void> {
-		@Override
-		protected Void doInBackground(SMS... params) {
-			SMS currentSMS = params[0];
-			currentSMS.setUsed(true);
-			MySQLiteHelper.getInstance(SMSDetailActivity.this).updateSMS(
-					currentSMS);
-			return null;
-		}
-	}
-
-	/**
-	 * update this sms to be used
-	 * 
-	 * @author dungnh8
-	 * 
-	 */
-	private class UpdateFavoritedSMSAsyncTask extends
-			AsyncTask<SMS, Void, Void> {
-		@Override
-		protected Void doInBackground(SMS... params) {
-			SMS currentSMS = params[0];
-			Log.i(TAG, currentSMS.toString());
-			MySQLiteHelper.getInstance(SMSDetailActivity.this).updateSMS(
-					currentSMS);
-			return null;
-		}
-	}
-
-	@Override
-	public void onFavoriteButtonChange(SMS sms) {
-		setImageFavorite(sms);
 	}
 }
--- SlidingMenu-master/SMSCute/src/com/kakaolabs/smscute/fragments/SMSDetailFragment.java
+++ SlidingMenu-master/SMSCute/src/com/kakaolabs/smscute/fragments/SMSDetailFragment.java
@@ -1,23 +1,25 @@
 package com.kakaolabs.smscute.fragments;
 
-import com.kakaolabs.smscute.R;
-import com.kakaolabs.smscute.database.MySQLiteHelper;
-import com.kakaolabs.smscute.database.table.SMS;
-import com.kakaolabs.smscute.helper.DrawHelper;
-import com.kakaolabs.smscute.util.Constants;
-
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.kakaolabs.smscute.R;
+import com.kakaolabs.smscute.database.MySQLiteHelper;
+import com.kakaolabs.smscute.database.table.SMS;
+import com.kakaolabs.smscute.util.Constants;
 
 public class SMSDetailFragment extends Fragment {
 	private TextView content;
 	private SMS sms;
+	private Button sendButton, likeButton;
 	private static final String TAG = "SMSDetailFragment";
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +28,7 @@ public class SMSDetailFragment extends Fragment {
 		setComponentView(v);
 		setArguments();
 		drawData();
+		setListener();
 		return v;
 	}
 
@@ -37,6 +40,8 @@ public class SMSDetailFragment extends Fragment {
 	private void setComponentView(View view) {
 		try {
 			content = (TextView) view.findViewById(R.id.sms_content_textview);
+			sendButton = (Button) view.findViewById(R.id.sms_content_send_sms);
+			likeButton = (Button) view.findViewById(R.id.sms_content_like_sms);
 		} catch (Exception e) {
 			Log.e(TAG, "setComponentView", e);
 		}
@@ -52,8 +57,8 @@ public class SMSDetailFragment extends Fragment {
 			sms = (SMS) getArguments().getSerializable(Constants.SMS);
 			sms = MySQLiteHelper.getInstance(getActivity()).getSMSByID(
 					sms.getId());
-			Log.i(TAG, sms.toString());
-			DrawHelper.drawFavoriteImageButton(sms);
+			// set favorite button
+			setFavoriteButton();
 		} catch (Exception e) {
 			Log.e(TAG, "setArguments", e);
 		}
@@ -66,11 +71,105 @@ public class SMSDetailFragment extends Fragment {
 	 */
 	private void drawData() {
 		try {
-			String contentText = "<p style='line-height:200%;'>"
-					+ sms.getContent() + "<p/>";
-			content.setText(Html.fromHtml(contentText));
+			content.setText(sms.getContent());
 		} catch (Exception e) {
 			Log.e(TAG, "drawData", e);
+		}
+	}
+
+	private void setListener() {
+		setFavoriteListener();
+		setSendListener();
+	}
+
+	/**
+	 * set send listener
+	 * 
+	 * @author dungnh8
+	 */
+	private void setSendListener() {
+		sendButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				SMS[] smsParams = { sms };
+				new UpdateUsedSMSAsyncTask().execute(smsParams);
+				// send sms
+				Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+				intent.putExtra("sms_body", sms.getContent());
+				intent.setType("vnd.android-dir/mms-sms");
+				startActivity(intent);
+			}
+		});
+	}
+
+	/**
+	 * set sms to be favorite
+	 * 
+	 * @author dungnh8
+	 */
+	private void setFavoriteListener() {
+		likeButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				boolean isFavorited = !sms.isFavorited();
+				sms.setFavorited(isFavorited);
+				// set button to be favorite
+				setFavoriteButton();
+				// update to database
+				SMS[] smsParams = { sms };
+				new UpdateFavoritedSMSAsyncTask().execute(smsParams);
+			}
+		});
+	}
+
+	/**
+	 * update this sms to be used
+	 * 
+	 * @author dungnh8
+	 * 
+	 */
+	private class UpdateUsedSMSAsyncTask extends AsyncTask<SMS, Void, Void> {
+		@Override
+		protected Void doInBackground(SMS... params) {
+			SMS currentSMS = params[0];
+			currentSMS.setUsed(true);
+			MySQLiteHelper.getInstance(SMSDetailFragment.this.getActivity())
+					.updateSMS(currentSMS);
+			return null;
+		}
+	}
+
+	/**
+	 * update this sms to be used
+	 * 
+	 * @author dungnh8
+	 * 
+	 */
+	private class UpdateFavoritedSMSAsyncTask extends
+			AsyncTask<SMS, Void, Void> {
+		@Override
+		protected Void doInBackground(SMS... params) {
+			SMS currentSMS = params[0];
+			Log.i(TAG, currentSMS.toString());
+			MySQLiteHelper.getInstance(SMSDetailFragment.this.getActivity())
+					.updateSMS(currentSMS);
+			return null;
+		}
+	}
+
+	/**
+	 * set favorite button
+	 * 
+	 * @author dungnh8
+	 */
+	private void setFavoriteButton() {
+		if (sms.isFavorited()) {
+			likeButton.setBackgroundResource(R.drawable.rounded_red_rectangle);
+		} else {
+			likeButton
+					.setBackgroundResource(R.drawable.rounded_green_rectangle);
 		}
 	}
 }
